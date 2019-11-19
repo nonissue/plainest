1a9cd814c55b7e704abef88124808190155fde0b
Andy Williams <andy@nonissue.org>
2019-11-18 23:11:29 -0700

    Added posts-read-latest to only get the latest posts document.

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,13 +1,92 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
-import { Button } from "@storybook/react/demo";
-import { ImageView } from "./components/ImageView";
+import { InstaGrid } from "./components/InstaGrid";
+import axios from "axios";
+import api from "./utils/api.js";
 
 export default { title: "Homepage" };
 
-export const SingleImage = () => (
+const postsMock = [
+  {
+    id: "2144811289880636561_20196334360",
+    link: "https://www.instagram.com/p/B3D5oEjlSyR/",
+    images: {
+      thumbnail: {
+        width: 150,
+        height: 150,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      },
+      low_resolution: {
+        width: 320,
+        height: 211,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      },
+      standard_resolution: {
+        width: 640,
+        height: 423,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      }
+    },
+    caption: "X."
+  },
+  {
+    id: "2144811289880636561_20196334360",
+    link: "https://www.instagram.com/p/B3D5oEjlSyR/",
+    images: {
+      thumbnail: {
+        width: 150,
+        height: 150,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      },
+      low_resolution: {
+        width: 320,
+        height: 211,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      },
+      standard_resolution: {
+        width: 640,
+        height: 423,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      }
+    },
+    caption: "X."
+  },
+  {
+    id: "2144811289880636561_20196334360",
+    link: "https://www.instagram.com/p/B3D5oEjlSyR/",
+    images: {
+      thumbnail: {
+        width: 150,
+        height: 150,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      },
+      low_resolution: {
+        width: 320,
+        height: 211,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      },
+      standard_resolution: {
+        width: 640,
+        height: 423,
+        url:
+          "https://scontent.cdninstagram.com/vp/be7f720fa62ac643b2289b110597f4f8/5E6AD647/t51.2885-15/sh0.08/e35/s640x640/69866223_443000699651907_1782646635031253506_n.jpg?_nc_ht=scontent.cdninstagram.com"
+      }
+    },
+    caption: "X."
+  }
+];
+
+export const ImageGrid = () => (
   <MemoryRouter initialEntries={["/images?src=test"]}>
-    <ImageView />
+    <InstaGrid posts={postsMock} />
   </MemoryRouter>
 );
 
--- src/lambda/posts-read-latest.js
+++ src/lambda/posts-read-latest.js
@@ -7,6 +7,8 @@ const client = new faunadb.Client({
   secret: "fnADdJQsv_ACCk1D6Izxwmf6TcbbjhLmbAGRdcGC"
 });
 
+// Ugly as hell way of doing this, but no worse than reversing the order?
+// could also just shift or pop or something?
 exports.handler = (event, context) => {
   console.log("Function `posts-read-all` invoked");
   return (
@@ -18,12 +20,11 @@ exports.handler = (event, context) => {
         console.log("Post refs", postRefs);
         console.log(`${postRefs.length} posts found`);
         console.log("postsRefs: " + postRefs);
-        // create new query out of post refs. http://bit.ly/2LG3MLg
-        const getAllPostDataQuery = postRefs.map(ref => {
-          return q.Get(ref);
-        });
+        const latestPostRef = postRefs[postRefs.length - 1];
+
+        const getLatestPost = q.Get(latestPostRef);
         // then query the refs
-        return client.query(getAllPostDataQuery).then(ret => {
+        return client.query(getLatestPost).then(ret => {
           return {
             statusCode: 200,
             body: JSON.stringify(ret)
