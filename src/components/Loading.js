73046425c4d50c48d21cde45da7b1d6b8f16b394
Andy Williams <andy@nonissue.org>
2019-11-19 23:59:24 -0700

    Loading converted to style components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const LoadingWrapper = styled.div`
+  font-family: 'Lekton', 'Courier', monospace;
+  font-style: italic;
+  animation: fadein 0.7s infinite;
+  color: #e2e8f0;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+
+  @keyframes fadein {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
 
 export function Loading() {
-  return <div className="loading">Loading...</div>;
+  return <LoadingWrapper>Loading...</LoadingWrapper>;
 }
+
+export default Loading;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,73 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion";
+import React from 'react';
+import styled from 'styled-components';
+import { Link, useLocation } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const NavWrapper = styled.div`
+  a,
+  a:link,
+  a:visited {
+    padding: 0;
+    display: block;
+    margin: 0;
+    line-height: 2em;
+    margin-right: 10px;
+    border-radius: 1.5em;
+    width: 2em;
+    height: 2em;
+    color: #fff;
+    font-weight: 600;
+    font-size: 1rem;
+    text-decoration: none;
+  }
+
+  a:hover {
+    border: 1px solid #fff;
+    background: #fff;
+    color: #333;
+  }
+
+  .nav-left {
+    position: absolute;
+    top: 5vh;
+    left: 4vh;
+  }
+
+  .nav-right {
+    position: absolute;
+    top: 5vh;
+    right: 4vh;
+  }
+`;
 
 export function Nav() {
   const location = useLocation();
 
   return (
-    <div className="nav">
-      {/* <div>
-        <Link to="/">Home</Link>
-      </div> */}
-      {location.pathname === "/" ? (
-        ""
-      ) : (
-        <AnimatePresence>
-          <motion.div
-            key="back"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
-            exit={{ opacity: 0.5, scale: 2, transition: { duration: 5 } }}
-            className="nav-left"
-          >
-            <Link to="/">{"<"}</Link>
-          </motion.div>
-        </AnimatePresence>
-      )}
-      <div className="nav-right">
-        <Link to="/about" alt="about page">
-          ?
-        </Link>
+    <NavWrapper>
+      <div className="nav">
+        {location.pathname === '/' ? (
+          ''
+        ) : (
+          <AnimatePresence>
+            <motion.div
+              key="back"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
+              exit={{ opacity: 0.5, scale: 2, transition: { duration: 5 } }}
+              className="nav-left"
+            >
+              <Link to="/">{'<'}</Link>
+            </motion.div>
+          </AnimatePresence>
+        )}
+        <div className="nav-right">
+          <Link to="/about" alt="about page">
+            ?
+          </Link>
+        </div>
       </div>
-    </div>
+    </NavWrapper>
   );
 }
+export default Nav;
